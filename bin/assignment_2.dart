1e0f84134cd737f65c324e161ef9d7699c5dd770
TAnjil002 <2222081075@uttarauniversity.edu.bd>
2026-09-25 20:11:10 +0600

    Add temperature conversion formulas

--- bin/assignment_2.dart
+++ bin/assignment_2.dart
@@ -6,4 +6,27 @@ void main() {
   print('====================================');
   print('       SMART TEMPERATURE CONVERTER');
   print('====================================');
+
+  double celsiusToFahrenheit(double celsius) {
+    return (celsius * 9 / 5) + 32;
+  }
+
+  double fahrenheitToCelsius(double fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
+
+  double celsiusToKelvin(double celsius) {
+    return celsius + 273.15;
+  }
+
+  double kelvinToCelsius(double kelvin) {
+    return kelvin - 273.15;
+  }
+
+  double fahrenheitToKelvin(double fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9 + 273.15;
+  }
+
+  double kelvinToFahrenheit(double kelvin) {
+    return (kelvin - 273.15) * 9 / 5 + 32;
 }
